b52f340c4bfc7ff006ccb56f9e5b8fbefaa485f7
dagreatcode <dagreatcode@gmail.com>
2021-01-28 09:53:52 -0500

    fixed err, forgot to require ('path')

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const path = require(path);
+const path = require("path");
 
 const PORT = process.env.PORT || 3001;
 

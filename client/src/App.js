179cac5f3e8dd5a631d1c19152b13516d4400d55
dagreatcode <dagreatcode@gmail.com>
2021-01-28 09:24:28 -0500

    comb before build

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,19 @@
-import {useEffect} from "react";
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 import axios from "axios";
 
 function App() {
-
   useEffect(() => {
     console.log("Make an API call");
-    axios.get("/api/config").then((response)=>{
-      console.log(response);
-    })
-    .catch((err)=>{
-      console.log(err);
-    });
+    axios
+      .get("/api/config")
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }, []);
   return (
     <div className="App">

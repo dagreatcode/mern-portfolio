5885b9e26c742927848096b6ecda1cac49439ca7
dagreatcode <dagreatcode@gmail.com>
2021-01-28 09:13:33 -0500

    fixed .catch in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,15 @@ import axios from "axios";
 
 function App() {
 
-
-useEffect(() => {
-  console.log("Make an API call");
-  axios.get("/api/config").then((response)=>{
-    console.log(response);
-}, [])
+  useEffect(() => {
+    console.log("Make an API call");
+    axios.get("/api/config").then((response)=>{
+      console.log(response);
+    })
+    .catch((err)=>{
+      console.log(err);
+    });
+  }, []);
   return (
     <div className="App">
       <header className="App-header">

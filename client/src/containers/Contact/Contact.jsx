f827df30abb04afa9b59ab14c85bf71f1c5e8c04
dagreatcode <dagreatcode@gmail.com>
2021-01-29 14:31:58 -0500

    added contact layout.

--- client/src/containers/Contact/Contact.jsx
+++ client/src/containers/Contact/Contact.jsx
@@ -1,11 +1,43 @@
-import React, { Component } from "react"; 
+import React, { Component } from "react";
 
 class Contact extends Component {
   render() {
     return (
-     <div>
-       <h1>Contact</h1>
-     </div>
+      <div>
+        <h1>Contact</h1>
+        <div className="mb-3">
+          <label for="exampleFormControlInput1" className="form-label">
+            Name
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleFormControlInput1"
+            placeholder="name@example.com"
+          />
+        </div>
+        <div className="mb-3">
+          <label for="exampleFormControlInput1" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleFormControlInput1"
+            placeholder="name@example.com"
+          />
+        </div>
+        <div className="mb-3">
+          <label for="exampleFormControlTextarea1" className="form-label">
+            What Would You Like?
+          </label>
+          <textarea
+            className="form-control"
+            id="exampleFormControlTextarea1"
+            rows="3"
+          ></textarea>
+        </div>
+      </div>
     );
   }
 }

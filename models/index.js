e908ca26e476923909f3e862b57a428b16a71701
dagreatcode <dagreatcode@gmail.com>
2021-03-04 19:42:32 -0500

    seeds works

--- models/index.js
+++ models/index.js
@@ -1,3 +1,3 @@
-model.exports = {
+module.exports = {
     Projects: require("./Projects")
 };
--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const db = require("../models");
-const image = require("../client/src/containers/Portfolio/img")
+// const image = require("../client/src/containers/Portfolio/img")
 
 // This file empties the projects collection and inserts the projects below
 
@@ -9,54 +9,54 @@ mongoose.connect(
   "mongodb://localhost/mern-portfolio"
 );
 
-const projectsSeed = [
+const projectSeed = [
   {
     name: "Book Save",
     link: "https://vince-books.herokuapp.com/",
     image:
-      "image/p1.png",
+      "",
     date: new Date(Date.now())
   },
   {
     name: "Shop",
     link: "https://dagreatcode.github.io/shop/",
     image:
-      "image/p2.png",
+      "",
     date: new Date(Date.now())
   },
   {
     name: "RetroLuxe",
     link: "https://retroluxe.herokuapp.com/",
     image:
-      "image/p3.png",
+      "",
     date: new Date(Date.now())
   },
   {
     name: "GoodAutoDash",
     link: "https://dagreatcode.github.io/goodAutoDash/",
     image:
-      "image/p4.png",
+      "",
     date: new Date(Date.now())
   },
   {
     name: "What's For Dinner",
     link: "https://dagreatcode.github.io/whats-for-dinner/",
     image:
-      "image/p5.png",
+      "",
     date: new Date(Date.now())
   },
   {
     name: "Positive Thoughts",
     link: "http://www.positive-thoughts.org/",
     image:
-      "image/p6.png",
+      "",
     date: new Date(Date.now())
   },
 ];
 
 db.Projects
   .remove({})
-  .then(() => db.Projects.collection.insertMany(ProjectsSeed))
+  .then(() => db.Projects.collection.insertMany(projectSeed))
   .then(data => {
     console.log(data.result.n + " records inserted!");
     process.exit(0);

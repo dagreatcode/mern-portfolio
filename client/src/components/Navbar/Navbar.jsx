045b6d5f9a9d4e2a5ec6dff7ee43da7b52a94f93
dagreatcode <dagreatcode@gmail.com>
2021-03-19 17:15:17 -0400

    redeploy for update

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -25,17 +25,29 @@ const Navbar = () => {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <NavLink to="/" className="nav-link" style={{ color:'white', fontWeight: 'bold'}}>
+              <NavLink
+                to="/"
+                className="nav-link"
+                style={{ color: "white", fontWeight: "bold" }}
+              >
                 Home
               </NavLink>
             </li>
             <li className="nav-item">
-              <NavLink to="/portfolio" className="nav-link" style={{ color:'white', fontWeight: 'bold'}}>
+              <NavLink
+                to="/portfolio"
+                className="nav-link"
+                style={{ color: "white", fontWeight: "bold" }}
+              >
                 Portfolio
               </NavLink>
             </li>
             <li className="nav-item">
-              <NavLink to="/contact" className="nav-link" style={{ color:'white', fontWeight: 'bold'}}>
+              <NavLink
+                to="/contact"
+                className="nav-link"
+                style={{ color: "white", fontWeight: "bold" }}
+              >
                 Contact
               </NavLink>
             </li>
